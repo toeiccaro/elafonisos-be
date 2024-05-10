930870a5400b0a3d4b710d7ec8da5ff6ded6a8c6
hoang <hoangthanh.nguyen@team.enouvo.com>
2024-05-10 23:55:43 +0700

    update migrate file

--- database/migrations/20240510111836_test.js
+++ database/migrations/20240510111836_test.js
@@ -1,25 +1,27 @@
 /**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
+ * @param {import("knex").Knex} knex
+ * @returns {Promise<void>}
  */
 exports.up = async function(knex) {
-    const sql = 
-    `CREATE TABLE IF EXISTS "userTest" (
-      id SERIAL PRIMARY KEY,
-      username VARCHAR(50) NOT NULL,
-      email VARCHAR(100) NOT NULL,
-      password VARCHAR(100) NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  );
-  `
-    await knex.raw(sql);
-  };
-  
-  exports.down = async function(knex) {
-    const sql = 
-    `DROP TABLE IF EXISTS "userTest";
-  `
-    await knex.raw(sql);
-    
-  };
-  
+  // const sql = `
+  //     CREATE TABLE IF NOT EXISTS "userTest" (
+  //         id SERIAL PRIMARY KEY,
+  //         username VARCHAR(50) NOT NULL,
+  //         email VARCHAR(100) NOT NULL,
+  //         password VARCHAR(100) NOT NULL,
+  //         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  //     );
+  // `;
+  // await knex.raw(sql);
+};
+
+/**
+* @param {import("knex").Knex} knex
+* @returns {Promise<void>}
+*/
+exports.down = async function(knex) {
+  const sql = `
+      DROP TABLE IF EXISTS "userTest";
+  `;
+  await knex.raw(sql);
+};
